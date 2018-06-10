4fc83e892de96c17c1a0597cea06388a1bbf398c
SweeneyTheBarber <37616255+SweeneyTheBarber@users.noreply.github.com>
2018-06-10 10:59:51 +0430

    Route changes

--- modules/controllers/appApi/PostController.js
+++ modules/controllers/appApi/PostController.js
@@ -4,7 +4,7 @@ const User = require(`${config.path.model}/user`);
 
 module.exports = new class SampleController extends Controller {
     index(req , res) {
-      User.findById(req.params.id, (err, user) => {
+      User.findById(req.user._id, (err, user) => {
         Post.find({user_id:user.following}, (err, posts) => {
           if(err){
             res.send(err);
@@ -15,10 +15,18 @@ module.exports = new class SampleController extends Controller {
     }
 
     add(req, res) {
+      req.checkBody('title' , 'وارد کردن فیلد تیتر الزامیست').notEmpty();
+      req.checkBody('body' , 'وارد کردن فیلد متن الزامیست').notEmpty();
+
+      this.escapeAndTrim(req , 'title body');
+
+      if(this.showValidationErrors(req, res))
+          return;
+
       this.model.Post({
           title : req.body.title,
           body : req.body.body,
-          user_id: req.params.id
+          user_id: req.user._id
       }).save(err => {
           if(err) {
             res.send(err);
@@ -31,7 +39,7 @@ module.exports = new class SampleController extends Controller {
   }
 
     come(req, res) {
-      Post.findById(req.params.id, (err, post) => {
+      Post.findById(req.params.post_id, (err, post) => {
         if(err){
           res.send(err);
         }
